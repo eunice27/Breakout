941b70f0f4a73cdb13a96f024c88057fedfd6c43
eunice27 <ebalderasy@gmail.com>
2021-04-05 23:10:39 -0500

    formulario

---
+++ form-contacto.php
@@ -0,0 +1,19 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$destinatario = 'eunicebalderasy@gmail.com';
+$header = "Enviado desde la pagina Breakout";
+
+// $mensajeCompleto = $message . "\nAtentamente: " . $nombre;
+$mensajeContacto = "De: $nombre \n";
+$mensajeContacto .= "Correo: $correo \n";
+$mensajeContacto .= "Mensaje: $mensaje \n";
+
+mail($destinatario, $header, $mensajeContacto);
+echo "<script>alert('Correo enviado exitosamente')</script>";
+echo "<script>setTimeout(\"location.href= 'index.html'\",1000)</script>";
+
+?>
